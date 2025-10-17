08544aa7989b5ab71bd00e99d87745e0d29ff118
JasonMun7 <jason.mun484@gmail.com>
2025-10-16 21:52:53 -0400

    adding in new side panel form

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "src/background.ts"),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === "background"
+            ? "background.js"
+            : "assets/[name]-[hash].js";
+        },
+      },
+    },
+  },
 });
